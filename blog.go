794f69e4287eebf0a01cb6feac57a7da0a5bcae6
Jamie Mansfield <dev@jamierocks.uk>
2016-08-13 19:21:07 +0100

    Import modules and models from WebRocks

--- blog.go
+++ blog.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+    "github.com/jamierocks/blog/models"
+    "github.com/jamierocks/blog/modules"
+)
 
+func main() {
+    // Initialise config
+    modules.InitConfig()
+
+    // Initialise database
+    modules.InitDatabase()
+    models.AutoMigrate()
 }
---
+++ models/post.go
@@ -0,0 +1,51 @@
+package models
+
+import (
+    "time"
+    "github.com/jamierocks/webrocks/modules"
+)
+
+type Post struct {
+    ID int64 `gorm:"primary_key"`
+    Title string
+    Slug string
+    Content string `gorm:"type:mediumtext"`
+
+    Author Author
+    AuthorID int64
+
+    CreatedAt time.Time
+    UpdatedAt time.Time
+}
+
+func (b Post) GetAuthor() Author {
+    var author Author
+    modules.DB.Model(&b).Related(&author)
+    return author
+}
+
+func GetPosts() []Post {
+    var posts []Post
+    modules.DB.Find(&posts)
+    return posts
+}
+
+func GetPost(slug string) Post {
+    var post Post
+    modules.DB.Where(&Post{Slug: slug}).FirstOrInit(&post)
+    return post
+}
+
+type PostSlice []Post
+
+func (p PostSlice) Len() int {
+    return len(p)
+}
+
+func (p PostSlice) Less(i, j int) bool {
+    return p[i].CreatedAt.Before(p[j].CreatedAt)
+}
+
+func (p PostSlice) Swap(i, j int) {
+    p[i], p[j] = p[j], p[i]
+}
